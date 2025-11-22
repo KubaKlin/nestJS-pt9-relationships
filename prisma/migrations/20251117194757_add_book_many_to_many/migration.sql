3b408f3ab9a99bacdf947de3972aff640d2c853d
Kuba Klin <poczta@kubaklin.pl>
2025-11-22 15:36:45 +0100

    feat: Remove `createdAt` and `publishedAt` fields from `Book` model with schema and migration adjustments

--- prisma/migrations/20251117194757_add_book_many_to_many/migration.sql
+++ prisma/migrations/20251117194757_add_book_many_to_many/migration.sql
@@ -3,8 +3,6 @@ CREATE TABLE "Book" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT,
-    "publishedAt" TIMESTAMP(3),
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
 );
